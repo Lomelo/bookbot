a3ffef4490b500e57277e1d58acdd063db0091ee
Lomelo <lomeloseugasala@outlook.com>
2024-12-28 21:32:09 -0700

    got rid of report function, sorted the main function to handle it. Looks done.

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def main():
     character_dictionary = create_char_dic(contents)
     count = word_count(contents)
     total_characters = character_count(character_dictionary)
-    print(report(book_path, count, contents))
+    sorted_list = dict_list(character_dictionary)
+    print(f"--- Begin report of {book_path} ---", f"{count} words found in the document")
+    for i in sorted_list:
+        print(f"The {i['char']} character was found {i["count"]} times")
+    print("--- End Report ---")
 
 def book_content(path):
     with open(path) as f:
@@ -18,7 +22,7 @@ def create_char_dic(contents):
     letter_count = {}
     for letter in (contents):
         lowered_letter = letter.lower()
-        if lowered_letter not in letter_count:
+        if lowered_letter not in letter_count and lowered_letter.isalpha() == True:
             letter_count[lowered_letter] = 1
         elif lowered_letter in letter_count:
             letter_count[f"{lowered_letter}"] += 1
@@ -31,12 +35,15 @@ def character_count(dictionary):
     return total
 
 def grab_dict_number(dictionary):
-    return dictionary["p"]
+    return dictionary["count"]
 
-def report(path, word_count, contents):
-    headline = f"Begin report of {path}"
-    provide_word_total = f"{word_count} words found in the document"
-    return headline, provide_word_total
+def dict_list(dictionary):
+    char_list = []
+    for key, value in dictionary.items():
+        char_dict = {"char": key, "count": value}
+        char_list.append(char_dict)
+    char_list.sort(reverse=True, key=grab_dict_number)
+    return char_list
 
 
 main()
