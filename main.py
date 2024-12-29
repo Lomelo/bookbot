5936483fe35ce0dc7630437ad82e3178236c6a99
Lomelo <lomeloseugasala@outlook.com>
2024-12-28 20:10:59 -0700

    separated function into two, one for dictionary and other for count

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     contents = book_content(book_path)
+    character_dictionary = create_char_dic(contents)
     count = word_count(contents)
-    total_characters = character_count(contents)
+    total_characters = character_count(character_dictionary)
     print(report(book_path, count, contents))
 
 def book_content(path):
@@ -13,17 +14,20 @@ def word_count(contents):
     words = contents.split()
     return len(words)
 
-def character_count(contents):
+def create_char_dic(contents):
     letter_count = {}
-    total = 0
     for letter in (contents):
         lowered_letter = letter.lower()
         if lowered_letter not in letter_count:
             letter_count[lowered_letter] = 1
         elif lowered_letter in letter_count:
             letter_count[f"{lowered_letter}"] += 1
-    for i in (letter_count):
-        total += letter_count[i]
+    return letter_count
+
+def character_count(dictionary):
+    total = 0
+    for i in (dictionary):
+        total += dictionary[i]
     return total
 
 def grab_dict_number(dictionary):
@@ -33,8 +37,6 @@ def report(path, word_count, contents):
     headline = f"Begin report of {path}"
     provide_word_total = f"{word_count} words found in the document"
     return headline, provide_word_total
-
-
 
 
 main()
